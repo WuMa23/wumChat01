4052d74f7dcb2f769784941ecd5d8cfa0253dd13
WuMa23 <WuMa23@gmail.com>
2013-12-22 22:24:17 +0100

    * ChatLog-Filename mit Tagesdatum * Doku hinzugefügt

--- FirstONE/WebContent/script.js
+++ FirstONE/WebContent/script.js
@@ -1,21 +1,27 @@
 /***/ 
-var xmlhttp = new XMLHttpRequest(); // Creating a new XMLHttpRequest object
+// Mit dem XMLHttpRequest-Objekt wird eine Anfrage an den Web-Server erstellt
+var xmlhttp = new XMLHttpRequest();
 
+// function zur Kommunikation mit dem Web-Server:
+// ein neuer Chat-Eintrag kann (muss aber nicht) an den Server gesendet werden
+// sämtliche Chat-Einträge werden vom Server zurückgemeldet
 function sndRcvHTTP (userMsg, userId) {
-	xmlhttp.onreadystatechange = function() {
+	xmlhttp.onreadystatechange = function() { // function für Verarbeitung der Server-Rückmeldung
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {	
-			$("#chatbox").html(xmlhttp.responseText);
-			$("#chatbox")[0].scrollTop = $("#chatbox")[0].scrollHeight;
+			$("#chatbox").html(xmlhttp.responseText); // Serverantwort in die ChatBox schreiben
+			$("#chatbox")[0].scrollTop = $("#chatbox")[0].scrollHeight; // ans Ende scrollen
 		};
 	};
-	var sndUrl = "http://localhost:8080/FirstONE/Server";
+	var sndUrl = "http://localhost:8080/FirstONE/Server";  
+	// Server-Servlet kann auch ohne usermsg aufgerufen werden: 
 	if (userMsg != undefined) {
 		sndUrl+="?usermsg="+ userMsg + "&userid=" + userId;
 	};
-	xmlhttp.open("GET",sndUrl,true);
-	xmlhttp.send(null);
+	xmlhttp.open("GET",sndUrl,true); 	// GET-Anfrage wird asynchron gesendet
+	xmlhttp.send(null);					// bei GET werden Daten mit URL übergeben 
 }
 
+// function zum Prüfen der Eingabe-Daten und Aufruf der Kommunikation mit dem Web-Server
 function sndMsgToServer () {
 	var userId = $("#userid")[0];
 	if (userId.value == "") { alert("User-Eingabe ist leer");} 
@@ -23,15 +29,16 @@ function sndMsgToServer () {
 	if (userMsg.value == "") { alert("Message-Eingabe ist leer");} 
 	else {	
 		sndRcvHTTP(userMsg.value, userId.value);
-		userMsg.value="";
+		userMsg.value="";	// Zurücksetzen des Eingabefeldes
 	}
 }
 
+// function schickt Betätigung der  Eingabe-Taste Chat-Daten an den Web-Server
 function chkEnter(e) {
 	if (e.keyCode == 13) {
 		sndMsgToServer ();
 	}
 }
 
-/***/
-$( "#banner" ).click(function() {$( this ).fadeOut();});
+// Werbe-Banner ausblenden
+$("#banner").click(function() {$( this ).fadeOut();});
--- FirstONE/src/Server.java
+++ FirstONE/src/Server.java
@@ -34,44 +34,61 @@ public class Server extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+	    Date zeitstempel = new Date();
 		ServletContext sctx = getServletContext();
 
 		String userMsg =  request.getParameter("usermsg");
 		String userId =  request.getParameter("userid");
-		String filename = "/WEB-INF/chatLog.txt";
-		
+	    
+	    // Ablage bzw. Name für ChatLog-File festlegen
+	    SimpleDateFormat dateyyyyMMdd = new SimpleDateFormat("yyyyMMdd");
+	    String filename = "/WEB-INF/chatLog_" + dateyyyyMMdd.format(zeitstempel) + ".txt";			
+
+		// Servlet kann mit und ohne userMsg aufgerufen werden.
+		// Wenn keine userMsg mitgegeben wird, dann erfolgt kein Eintrag im chatLog-File
 		if (userMsg != null) {
-		    System.out.println("Chat-Nachricht: " + userMsg);
+
+			// userMSG-Ausgabe in Console
+			System.out.println("Chat-Nachricht: " + userMsg); 
 		    
-		    Date zeitstempel = new Date();
-		    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-		     
-		    String sctxPath = sctx.getRealPath("/");
-		    File logfile = new File(sctxPath, filename);
+		    String sctxPath = sctx.getRealPath("/");		// Dateipfad ServletContext ermitteln
+		    File logfile = new File(sctxPath, filename);	// Datei instanzieren (für exist-Prüfuen)	
 		    FileWriter out;
-		    if (!(logfile.exists())) {
-		    	out = new FileWriter(logfile);
-		    	out.write("neues file" + System.lineSeparator());
-		    	System.out.println(filename + " in " + sctxPath + " erstellt");
+		    if (!(logfile.exists())) {						 
+		    	out = new FileWriter(logfile);				// ChatLog-File neu erstellen
+		    	// Datum in die erste Zeile schreiben
+		    	SimpleDateFormat dateISO = new SimpleDateFormat("yyyy-MM-dd");
+		    	out.write("[" + dateISO.format(zeitstempel) + "]" + System.lineSeparator()); 
+		    	System.out.println(filename + " in " + sctxPath + " erstellt"); // Consol-Ausgabe
 		    } else {
-		    	out = new FileWriter(logfile, true);
+		    	out = new FileWriter(logfile, true);		// bestehendes ChatLog-File verwenden
 		    }
-		    out.write(simpleDateFormat.format(zeitstempel) + "|" + userId + "|" + userMsg + System.lineSeparator());
-		    out.close();
+
+		    // Zeit, User und Message in ChatLog-File schreiben
+		    SimpleDateFormat timeHHmmss = new SimpleDateFormat("HH:mm:ss");
+		    out.write(timeHHmmss.format(zeitstempel) + ";" + userId + ";" + userMsg + System.lineSeparator());
+		    out.close(); // close ist für nachfolgendes Lesen wichtig
 		}
 		
 		InputStream is = sctx.getResourceAsStream(filename);
         if (is != null) {
         	BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             PrintWriter writer = response.getWriter();
-            String text = "";
+            String textline = "";
             
-            // Datei lesen und in den Response schreiben:
-            while ((text = reader.readLine()) != null) {
-                writer.println("<div id='chatboxmsg'>" + text + "</div>");
+            // ChatLog-File lesen, Zeile in Felder aufsplitten und in den Response schreiben:
+            while ((textline = reader.readLine()) != null) {
+            	String[] textfeld = textline.split(";");
+            	String sndText = "<div id='chatboxmsg'>";
+            	if (textfeld.length > 0) {sndText += textfeld[0];} 
+            	if (textfeld.length > 1) {sndText += " <i>" + textfeld[1] + "</i>";}
+            	if (textfeld.length > 2) {sndText += " " + textfeld[2];}
+            	sndText += "</div>";
+                writer.println(sndText);
             }
             reader.close();
         }
+
 	}
 
 	/**
